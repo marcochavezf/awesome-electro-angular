1a3880cfebad831841633648318eb98e690547c1
Marco Chávez <marcochavezf@hotmail.com>
2016-10-05 22:46:58 -0500

    add jstree sample

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const electron = require('electron');
-
+// Module to control application life.
 const app = electron.app;
 
 // adds debug features like hotkeys for triggering dev tools and reload
@@ -16,29 +16,47 @@ function onClosed() {
 }
 
 function createMainWindow() {
+	// Create the browser window.
 	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
+		width: 800,
+		height: 600
 	});
 
+	// and load the index.html of the app.
 	win.loadURL(`file://${__dirname}/index.html`);
+
+	// Open the DevTools.
+	win.webContents.openDevTools()
+
+	// Emitted when the window is closed.
 	win.on('closed', onClosed);
 
 	return win;
 }
 
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
 
 app.on('activate', () => {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
 	if (!mainWindow) {
 		mainWindow = createMainWindow();
 	}
 });
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
 	mainWindow = createMainWindow();
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
