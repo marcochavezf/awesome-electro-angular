6415ff642220766a820735c23ad42c899cefccff
Marco Chávez <marcochavezf@hotmail.com>
2016-10-12 23:33:05 -0500

    Add loading gif and save last directory selected

--- renderer.js
+++ renderer.js
@@ -1,18 +1,42 @@
 /**
  * Created by marcochavezf on 10/8/16.
  */
+////////////////// Libraries
 const _ = require('lodash');
 const ipc = require('electron').ipcRenderer;
-var angularEsprimaFun = require('../angular-esprima-fun/lib');
+const Configstore = require('configstore');
+const pkg = require('./package.json');
+
+// create a Configstore instance with an unique ID e.g.
+// package name and optionally some default values
+const conf = new Configstore(pkg.name);
+const angularEsprimaFun = require('../angular-esprima-fun/lib');
+
+////////////////// Configuration
 
 //Open a directory
 const selectDirBtn = document.getElementById('select-directory');
 selectDirBtn.addEventListener('click', function (event) {
 	ipc.send('open-file-dialog')
 });
-ipc.on('selected-directory', function (event, path) {
-	document.getElementById('selected-file').innerHTML = `Dir. selected: ${path}`;
 
+ipc.on('selected-directory', selectedDirectoryEvent);
+
+//Check if there's path saved from last session
+var pathToFiles = conf.get('path');
+if (pathToFiles) {
+	selectedDirectoryEvent(null, JSON.parse(pathToFiles));
+}
+
+///////////////// Functions
+
+function selectedDirectoryEvent(event, path) {
+	document.getElementById('selected-file').innerHTML = `Dir. selected: ${path}`;
+	var loading = document.getElementById('loading');
+	loading.style.display = 'block';
+	$('#example').jstree('destroy');
+
+	conf.set('path', JSON.stringify(path));
 	angularEsprimaFun.createControllerSemantics(path[0], (controllerSemantics)=> {
 
 		var controllersFiles = controllerSemantics.controllerFiles;
@@ -20,28 +44,13 @@ ipc.on('selected-directory', function (event, path) {
 		var ctlrsJstreeData = createCtrlrsJstreeData(controllersFiles);
 		//Create the JSON jstree data config.
 		var jstreeConfig = createJstreeConfig(ctlrsJstreeData);
+
+		loading.style.display = 'none';
 		$(function() {
-			$('#example').jstree('destroy');
 			$('#example').jstree(jstreeConfig);
 		});
 	});
-});
-
-/*
-//var dirTest = '../angular-esprima-fun/test/file.examples/testB';
-var dirTest = '/Users/marcochavezf/GitHub/angular-esprima-fun/test/file.examples/testB';
-angularEsprimaFun.createControllerSemantics(dirTest, (controllerSemantics)=> {
-
-	var controllersFiles = controllerSemantics.controllerFiles;
-	//Get all controllers from controllerFiles into one array.
-	var ctlrsJstreeData = createCtrlrsJstreeData(controllersFiles);
-	//Create the JSON jstree data config.
-	var jstreeConfig = createJstreeConfig(ctlrsJstreeData);
-	$(function() {
-		$('#example').jstree(jstreeConfig);
-	});
-});
-*/
+}
 
 function createCtrlrsJstreeData(controllersFiles){
 	var controllers = _.reduce(controllersFiles, (controllers, controllerFile)=>{
